53521c1e26fb448571e1c97c84e58fa9aac34424
ecureuill <logika.sciuro@gmail.com>
2024-03-07 13:24:55 -0300

    🔧 add junit reporter

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,11 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     baseUrl: "https://starbugs-qa.vercel.app",
+    reporter: 'junit',
+    reporterOptions: {
+      mochaFile: 'results/output.xml',
+      toConsole: true,
+    },
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
