21d01c069bb7687cd2962eef3f8e1ee654230329
ecureuill <logika.sciuro@gmail.com>
2024-03-07 16:12:50 -0300

    🔧 add allure plugin, set viewport size, and enable video recording

--- cypress.config.js
+++ cypress.config.js
@@ -1,15 +1,21 @@
 const { defineConfig } = require("cypress");
+const allureWriter = require('@shelex/cypress-allure-plugin/writer');
 
 module.exports = defineConfig({
   e2e: {
     baseUrl: "https://starbugs-qa.vercel.app",
+    video: true,
     reporter: 'junit',
     reporterOptions: {
       mochaFile: 'results/output.xml',
       toConsole: true,
     },
+    viewportHeight: 1080,
+    viewportWidth: 1920,
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      allureWriter(on, config);
+      return config;
     },
   },
 });
